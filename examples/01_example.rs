7ee4747f441919029e28f5c0693c8c9e0f97aacc
Mathias Stadler <stadler-mathias@web.de>
2025-08-20 21:44:08 +0200

    finished for today

--- examples/01_example.rs
+++ examples/01_example.rs
@@ -1,34 +1,70 @@
+use eframe::egui;
 #[allow(unused_imports)]
-use egui::{Context,CentralPanel,Label, };
+use egui::{CentralPanel, Context, Label};
 
 // #[derive(Default)]
-struct MyApp {}
-
-impl MyApp {
-
-    pub fn new(_cc:&eframe::CreationContext<'_>) ->Self{
-
-        Default::default()
-
-    }
-    
+struct MyApp {
+    name: String,
+    age: u32,
 }
 
-impl  eframe::App for MyApp  {
+impl MyApp {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Default::default()
+        //std::default::Default()
+    }
+}
 
-    fn update(&mut self, ctx:&Context, _frame:&mut eframe::Frame) {
-        CentralPanel::default().show(ctx,|ui| {
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
             ui.label("ui label");
             ui.heading("ui.heading");
             ui.add(Label::new("ui.add"))
         });
     }
-    
 }
 
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
 
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
 
-fn main() -> Result<(),eframe::Error>{
-    eframe::run_native(MyApp,eframe::NativeOptions::default(),Box::new(|cc|Box::new(MyApp::new)))
+fn org_main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
 
+            Ok(Box::<MyApp>::default())
+        }),
+    )
 }
